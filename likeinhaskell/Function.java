a24a85b13718d4baf5a9e69459facd0be6d0f2aa
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-02-04 18:49:27 +0100

    Add solutions for likehaskell

--- likeinhaskell/Function.java
+++ likeinhaskell/Function.java
@@ -14,10 +14,27 @@ package likeinhaskell;
 //  .
 //  Hint: To solve this task, you need to use type parameters on both the
 //  interface itself, *and* on one of its methods.
-public interface Function {
-    // public ... apply(...);
+public interface Function<T,R> {
+    public R apply(T t);
+
     // TODO task 5: For even more challenge, really tricky!
     //  Give the compose() function a default implementation.
-    // public ... compose(...);
+    default <A> Function<A,R> compose(Function<A,T> other) {
+        return (a) -> this.apply(other.apply(a));
+    }
 
+    public static void main(String[] args) {
+        PlusOne plus1 = new PlusOne();
+        Integer i = 5;
+        System.out.println(i);
+        System.out.println(plus1.apply(i));
+        System.out.println(plus1.compose(plus1).apply(i));
+    }
+}
+
+class PlusOne implements Function<Integer,Integer> {
+    @Override
+    public Integer apply(Integer x) {
+        return x + 1;
+    }
 }
