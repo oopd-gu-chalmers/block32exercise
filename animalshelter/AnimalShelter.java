03949188a57eca40dcce78f77f44991fd5752b45
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-02-04 17:55:47 +0100

    Add shelter solution

--- animalshelter/AnimalShelter.java
+++ animalshelter/AnimalShelter.java
@@ -6,19 +6,19 @@ import java.util.List;
 /**
  * Created by Niklas on 2016-02-07.
  */
-public class AnimalShelter {
+public class AnimalShelter<A extends Animal> {
     // TODO task 2b: This is the class you primarily need to update, using
     //  parametric polymorphism, to ensure that shelters can be specialised
     //  to specific animals, with complete static type guarantees.
     //  .
     //  Hint: the subclasses CatShelter and DogShelter should become obsolete.
-    private List<Animal> animalsInShelter = new ArrayList<>(20);
+    private List<A> animalsInShelter = new ArrayList<A>(20);
 
-    Animal adoptAnimal(){
+    A adoptAnimal(){
         return animalsInShelter.remove(0);
     }
 
-    void putAnimal(Animal animal){
+    void putAnimal(A animal){
         animalsInShelter.add(animal);
     }
 
